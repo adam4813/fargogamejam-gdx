2313806938fa98e1fd3900c54e461a9de3e9d2f4
Adam Martin <adam.madram@gmail.com>
2019-10-05 11:59:11 -0500

    Add animated sprite and star assets loading

--- core/src/fgm/fgj/gamejamgame/screens/TitleScreen.java
+++ core/src/fgm/fgj/gamejamgame/screens/TitleScreen.java
@@ -3,29 +3,69 @@ package fgm.fgj.gamejamgame.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import fgm.fgj.gamejamgame.AnimatedSprite;
 import fgm.fgj.gamejamgame.GameJamGame;
 import fgm.fgj.gamejamgame.screens.menus.TitleMenu;
+
+import static fgm.fgj.gamejamgame.GameJamGame.SCREEN_HEIGHT;
+import static fgm.fgj.gamejamgame.GameJamGame.SCREEN_WIDTH;
 
 public class TitleScreen extends ScreenAdapter {
 	private final Stage stage;
 	private final TitleMenu titleMenu;
+	private final SpriteBatch spriteBatch;
+
+	private final ArrayList<AnimatedSprite> animatedStars = new ArrayList<>();
+	private GameJamGame game;
 
 	public TitleScreen(GameJamGame game) {
+		this.game = game;
 		stage = new Stage();
 		titleMenu = new TitleMenu(game);
+		spriteBatch = new SpriteBatch();
 		stage.addActor(titleMenu.getTable());
+	}
+
+	private Sprite buildStarSprite(Texture texture, float x, float y) {
+		Sprite sprite = new Sprite(texture, 64, 64);
+		sprite.setPosition(x, y);
+		return sprite;
+	}
+
+	Random random = new Random();
+
+	private void buildAnimatedStar(String filename) {
+		animatedStars.add(new AnimatedSprite(game.getAsset(filename),
+			random.nextFloat() * SCREEN_WIDTH, random.nextFloat() * SCREEN_HEIGHT, 64, 64, 15, .1f));
 	}
 
 	@Override
 	public void show() {
 		Gdx.input.setInputProcessor(stage);
+		buildAnimatedStar("data/stars/yellow.png");
+		buildAnimatedStar("data/stars/white.png");
+		buildAnimatedStar("data/stars/blue.png");
+		buildAnimatedStar("data/stars/orange.png");
+		buildAnimatedStar("data/stars/red.png");
 	}
 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		spriteBatch.begin();
+		for (AnimatedSprite animatedStar : animatedStars) {
+			animatedStar.draw(spriteBatch, delta);
+		}
+		spriteBatch.end();
 		stage.act(Gdx.graphics.getDeltaTime());
 		stage.draw();
 	}
