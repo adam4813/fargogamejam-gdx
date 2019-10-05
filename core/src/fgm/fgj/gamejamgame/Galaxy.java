c7e288a3bab0f107b18203cfea43c2b115cc1c27
Davin Loegering <davin.loegering@imaginemajesty.com>
2019-10-05 13:38:36 -0500

    Galaxy defined.

---
+++ core/src/fgm/fgj/gamejamgame/Galaxy.java
@@ -0,0 +1,9 @@
+package fgm.fgj.gamejamgame;
+
+import java.util.List;
+
+public class Galaxy {
+	private SolarSystem solarSystem;
+	private List<SolarSystem> solarSystems;
+	private SolarSystem shipLocation;
+}
