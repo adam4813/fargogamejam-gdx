22ee5303b2301d7f456e1d084ee07ade4d0912f0
Adam Martin <adam.madram@gmail.com>
2019-10-06 13:13:14 -0500

    Added "final" title screen

--- core/src/fgm/fgj/gamejamgame/screens/StarMapScreen.java
+++ core/src/fgm/fgj/gamejamgame/screens/StarMapScreen.java
@@ -179,6 +179,7 @@ public class StarMapScreen implements Screen {
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+		camera.update();
 		SpriteBatch spriteBatch = game.getSpriteBatch();
 		spriteBatch.setProjectionMatrix(camera.combined);
 		spriteBatch.begin();
