97f9ba7e7812cd09e86f6dffd2efc94edeedb8ef
Davin Loegering <davin.loegering@imaginemajesty.com>
2019-10-05 21:38:17 -0500

    Life support systems cleaned up.

--- core/src/fgm/fgj/gamejamgame/LifeSupportSystem.java
+++ core/src/fgm/fgj/gamejamgame/LifeSupportSystem.java
@@ -1,16 +1,35 @@
 package fgm.fgj.gamejamgame;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class LifeSupportSystem implements PartModules {
-	private int solarRadiationTolerance;
-	private List<AtmosphericComposition> supportedAtmosphereCompositions;
-	private int atmosphericEfficency;
+	final int solarRadiationTolerance;
+	final List<AtmosphericComposition> supportedAtmosphereCompositions;
+	/** Amount of gas consumed per crew member reduction. */
+	final int gasEfficiency;
 
-	public LifeSupportSystem (int solarRadiationTolerance, List<AtmosphericComposition> supportedAtmosphereCompositions, int atmosphericEfficency ) {
-		this.solarRadiationTolerance = solarRadiationTolerance;
+	public LifeSupportSystem (List<AtmosphericComposition> supportedAtmosphereCompositions, int solarRadiationTolerance, int gasConsumption ) {
+		if(supportedAtmosphereCompositions == null){
+			throw new IllegalArgumentException("A life support system cannot have a null supported compositions of atmosphere.");
+		}else if (supportedAtmosphereCompositions.isEmpty()){
+			throw new IllegalArgumentException("A life support system is worthless without supported atmosphere composition.");
+		}
 		this.supportedAtmosphereCompositions = supportedAtmosphereCompositions;
-		this.atmosphericEfficency = atmosphericEfficency;
+		this.solarRadiationTolerance = this.withinRangeOrDefault(solarRadiationTolerance, 0, 5, 2);
+		this.gasEfficiency = this.withinRangeOrDefault(gasConsumption, 0, 5, 0);
+	}
+	/**
+	 * @param value desired value
+	 * @param min lowest allowed value
+	 * @param max highest allowed value
+	 * @param def default value if desired value is out of range
+	 * @return value if valid, otherwise default
+	 */
+	private int withinRangeOrDefault(int value, int min, int max, int def) {
+		if (value >= min && value <= max) {
+			return value;
+		} else {
+			return def;
+		}
 	}
 }
